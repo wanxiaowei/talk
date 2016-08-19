1d78d3c8897da3e907988f5246753ca589c54c3b
wanxiaowei <wanxiaowei@papayamobile.cn>
2016-08-19 17:57:54 +0800

    first commit

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8  -*-
+import os
+import sys
+sys.setdefaultencoding('utf-8')
+DB_MYSQL_USER = 'root'
+DB_MYSQL_PASSWORD = 'root'
+
---
+++ talk.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS talk;
+USE talk;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS permission;
+CREATE TABLE user (id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                   name VARCHAR(100) NOT NULL,
+                   password VARCHAR(100) NOT NULL,
+                   permission_level INTEGER NOT NULL DEFAULT 3);
+CREATE TABLE permission(level INTEGER NOT NULL,
+                        context VARCHAR(100) NOT NULL);
+INSERT INTO permission VALUES (1,'administer'),(2,'boss'),(3,'human being');
